2dd0664d7dd04295daec413066d4e7a768f17371
philrough <44094086+philrough@users.noreply.github.com>
2022-06-09 08:56:28 +0100

    Create db.py

---
+++ db.py
@@ -0,0 +1,6 @@
+countries = [
+    {"id": 1, "name": "Error", "capital": "Error", "area": 000000},
+    {"id": 2, "name": "Thailand", "capital": "Bangkok", "area": 513120},
+    {"id": 3, "name": "Australia", "capital": "Canberra", "area": 7617930},
+    {"id": 4, "name": "Egypt", "capital": "Cairo", "area": 1010408},
+]
